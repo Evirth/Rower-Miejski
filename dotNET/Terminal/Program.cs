8daaf40dbc04faa853af56c17e2eb6482094aa2b
Bartosz Hliwa <bartosz.hliwa@gmail.com>
2017-05-10 19:08:43 +0200

    Created projects for all 3 apps

---
+++ dotNET/Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terminal for city bikes project");
+        }
+    }
+}
