c8eee845db45978fdb8af23a59c05431b05296b5
Bartosz Hliwa <bartosz.hliwa@gmail.com>
2017-05-14 20:01:44 +0200

    Widok i model formularza logowania
    1. Oba projekty zmienione na .NET Core
    2. Model danych logowania z walidacją
    3. Widok logowania z walidacją po stronie klienta
    4. Resources
    5. Trochę JSa

---
+++ dotNET/Admin/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Admin.Models
+{
+    public class Login
+    {
+        [JsonProperty("username")]
+        [Required(ErrorMessageResourceType = typeof(Resources.Errors.ModelDataValidation), ErrorMessageResourceName = "EmptyField")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        [Required(ErrorMessageResourceType = typeof(Resources.Errors.ModelDataValidation), ErrorMessageResourceName = "EmptyField")]
+        public string Password { get; set; }
+    }
+}
